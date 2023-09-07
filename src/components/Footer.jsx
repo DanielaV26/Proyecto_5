7905da3d6bc6ac1111f8897b6ea0d5b63b5921f8
Carolina Hernandez <hernandez.carolina@live.cl>
2023-09-07 13:16:11 -0300

    [feat]: Galery

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,16 +9,16 @@ export const Footer = () => {
                     <div className="md:flex md:justify-between">
                         <div className="mb-6 md:mb-0">
                             <a href="https://flowbite.com/" className="flex items-center">
-                                <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="FlowBite Logo" />
-                                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                                <img src="https://res.cloudinary.com/dt7h6qci4/image/upload/v1694101705/candle_kawkae.png" className="h-8 mr-3" alt="FlowBite Logo" />
+                                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">BrightBooks</span>
                             </a>
                         </div>
                         <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                             <div>
-                                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
+                                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Nosotros</h2>
                                 <ul className="text-gray-500 dark:text-gray-400 font-medium">
                                     <li className="mb-4">
-                                        <a href="https://flowbite.com/" className="hover:underline">Flowbite</a>
+                                        <a href="https://flowbite.com/" className="hover:underline">Nuestra historia</a>
                                     </li>
                                     <li>
                                         <a href="https://tailwindcss.com/" className="hover:underline">Tailwind CSS</a>
@@ -26,24 +26,24 @@ export const Footer = () => {
                                 </ul>
                             </div>
                             <div>
-                                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
+                                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
                                 <ul className="text-gray-500 dark:text-gray-400 font-medium">
                                     <li className="mb-4">
-                                        <a href="https://github.com/themesberg/flowbite" className="hover:underline ">Github</a>
+                                        <a href="https://github.com/themesberg/flowbite" className="hover:underline ">Terminos y condiciones</a>
                                     </li>
                                     <li>
-                                        <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">Discord</a>
+                                        <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">Politicas de privacidad</a>
                                     </li>
                                 </ul>
                             </div>
                             <div>
-                                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
+                                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Contacto</h2>
                                 <ul className="text-gray-500 dark:text-gray-400 font-medium">
                                     <li className="mb-4">
-                                        <a href="#" className="hover:underline">Privacy Policy</a>
+                                        <a href="#" className="hover:underline">+56965678219</a>
                                     </li>
                                     <li>
-                                        <a href="#" className="hover:underline">Terms &amp; Conditions</a>
+                                        <a href="#" className="hover:underline">contacto@brightbooks.cl</a>
                                     </li>
                                 </ul>
                             </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,34 +1,6 @@
 
-// import { DivCategoria } from "./DivCategoria"
 import { CarruselFlow } from "./CarruselFlow"
 
-
-// const categorias =[
-//   {
-//     categoria:"VELAS",
-//     imagen:"https://res.cloudinary.com/dt7h6qci4/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1693055602/WhatsApp_Image_2023-08-24_at_18.47.16_tfhbmi.jpg"
-//   },
-//   {
-//     categoria:"LIBROS",
-//     imagen:"https://res.cloudinary.com/dt7h6qci4/image/upload/v1693326193/WhatsApp_Image_2023-08-24_at_19.40.09_re43yc.jpg"
-//   },
-//   {
-//     categoria:"MARCADORES",
-//     imagen:"https://res.cloudinary.com/dt7h6qci4/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1693233859/B1_neqh6l.jpg"
-//   },
-//   {
-//     categoria:"3D BOOK",
-//     imagen:"https://res.cloudinary.com/dt7h6qci4/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1693055602/WhatsApp_Image_2023-08-24_at_19.04.08_t9tkel.jpg"
-//   },
-//   {
-//     categoria:"TAZAS",
-//     imagen:"https://res.cloudinary.com/dt7h6qci4/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1693233820/Bright5_tdbvdo.jpg"
-//   },
-//   {
-//     categoria:"VARIOS",
-//     imagen:"https://res.cloudinary.com/dt7h6qci4/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1693055602/WhatsApp_Image_2023-08-24_at_19.38.57_2_pbfyyf.jpg"
-//   }
-// ]
 
 export const Home = () => {
   return (
@@ -37,49 +9,34 @@ export const Home = () => {
     <CarruselFlow/>
     
 <div  className="flex items-center justify-center py-4 md:py-8 flex-wrap">
-    <button type="button"  className="text-blue-700 hover:text-white border border-blue-600 bg-white hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-full text-base font-medium px-5 py-2.5 text-center mr-3 mb-3 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:bg-gray-900 dark:focus:ring-blue-800">All categories</button>
-    <button type="button"  className="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center mr-3 mb-3 dark:text-white dark:focus:ring-gray-800">Shoes</button>
-    <button type="button"  className="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center mr-3 mb-3 dark:text-white dark:focus:ring-gray-800">Bags</button>
-    <button type="button"  className="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center mr-3 mb-3 dark:text-white dark:focus:ring-gray-800">Electronics</button>
-    <button type="button"  className="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center mr-3 mb-3 dark:text-white dark:focus:ring-gray-800">Gaming</button>
+    <button type="button"  className="text-blue-700 hover:text-white border border-blue-600 bg-white hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-full text-base font-medium px-5 py-2.5 text-center mr-3 mb-3 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:bg-gray-900 dark:focus:ring-blue-800">Categorías</button>
+    <button type="button"  className="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center mr-3 mb-3 dark:text-white dark:focus:ring-gray-800">Velas</button>
+    <button type="button"  className="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center mr-3 mb-3 dark:text-white dark:focus:ring-gray-800">Libros</button>
+    <button type="button"  className="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center mr-3 mb-3 dark:text-white dark:focus:ring-gray-800">Marcadores</button>
+    <button type="button"  className="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center mr-3 mb-3 dark:text-white dark:focus:ring-gray-800">Llaveros</button>
+    <button type="button"  className="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center mr-3 mb-3 dark:text-white dark:focus:ring-gray-800">Tazas</button>
+    <button type="button"  className="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center mr-3 mb-3 dark:text-white dark:focus:ring-gray-800">Varios</button>
 </div>
 <div  className="grid grid-cols-2 md:grid-cols-3 gap-4">
     <div>
-        <img  className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt=""/>
+        <img  className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dt7h6qci4/image/upload/v1693341705/velas_y_mas/WhatsApp_Image_2023-08-24_at_18.47.16_m1wjxy.jpg" alt="velas"/>
     </div>
     <div>
-        <img  className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt=""/>
+        <img  className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dt7h6qci4/image/upload/v1694098996/1277_cifspv.jpg" alt="libros"/>
     </div>
     <div>
-        <img  className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt=""/>
+        <img  className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dt7h6qci4/image/upload/v1693341694/velas_y_mas/B1_ihr5mr.jpg" alt="Marcadores"/>
     </div>
     <div>
-        <img  className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt=""/>
+        <img  className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dt7h6qci4/image/upload/v1693055602/WhatsApp_Image_2023-08-24_at_19.04.08_t9tkel.jpg" alt="Llaveros"/>
     </div>
     <div>
-        <img  className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt=""/>
+        <img  className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dt7h6qci4/image/upload/v1693233820/Bright5_tdbvdo.jpg" alt="tazas"/>
     </div>
     <div>
-        <img  className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" alt=""/>
+        <img  className="h-auto max-w-full rounded-lg" src="https://res.cloudinary.com/dt7h6qci4/image/upload/v1693341698/velas_y_mas/bright45_z1jrtk.jpg" alt="Varios"/>
     </div>
-    <div>
-        <img  className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg" alt=""/>
-    </div>
-    <div>
-        <img  className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg" alt=""/>
-    </div>
-    <div>
-        <img  className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg" alt=""/>
-    </div>
-    <div>
-        <img  className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg" alt=""/>
-    </div>
-    <div>
-        <img  className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg" alt=""/>
-    </div>
-    <div>
-        <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg" alt=""/>
-    </div>
+    
 </div>
 
     {/* <div className="dibujoVela">
