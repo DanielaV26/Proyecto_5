0c6c63032401e1f935e71726285113b20b014a03
DanielaV26 <danielavargasgonzalez.v@gmail.com>
2023-09-21 10:14:57 -0300

    [feat:user]

---
+++ src/components/UserSvg.jsx
@@ -0,0 +1,18 @@
+export const UserSvg = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24" className="w-10 h-10"
+    {...props}
+  >
+    <g stroke="#865a96" strokeWidth={1.5}>
+      <circle cx={12} cy={9} r={3} />
+      <circle cx={12} cy={12} r={10} />
+      <path
+        strokeLinecap="round"
+        d="M17.97 20c-.16-2.892-1.045-5-5.97-5s-5.81 2.108-5.97 5"
+      />
+    </g>
+  </svg>
+)
+
