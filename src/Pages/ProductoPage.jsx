5f48defb1e1e829ff24de3c3c482a5c17e504283
Carolina Hernandez <hernandez.carolina@live.cl>
2023-09-24 22:03:35 -0300

    [fix]:Cambios productopage

--- src/Pages/ProductoPage.jsx
+++ src/Pages/ProductoPage.jsx
@@ -3,28 +3,28 @@ import { Link, useParams } from "react-router-dom"
 import axios from "axios"
 
 export const ProductoPage = () => {
-    const param = useParams()
-    const id = param.id
-    const [producto, setProducto] = useState({})
-    const getProducto = async () => {
-      const url = `https://proyecto5-backend-e5u3-dev.fl0.io/products/product/${id}`
-      const {data} = await axios.get(url, {
-    
-        headers:{
-          "Access-Control-Allow-Origin": "*"
-        }
-          })
-      setProducto(data.producto)
-    }
-    useEffect(() => {
+  const param = useParams()
+  const id = param.id
+  const [producto, setProducto] = useState({})
+  const getProducto = async () => {
+    const url = `https://proyecto5-backend-e5u3-dev.fl0.io/products/product/${id}`
+    const { data } = await axios.get(url, {
 
-      getProducto()
-    
-    }, [])
-    
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      }
+    })
+    setProducto(data.producto)
+  }
+  useEffect(() => {
+
+    getProducto()
+
+  }, [])
+
   return (
     <>
-    {/* <!--
+      {/* <!--
   This example requires some changes to your config:
   
   ```
@@ -45,80 +45,81 @@ export const ProductoPage = () => {
   }
   ```
 --> */}
-<div className="bg-white">
-  <div className="pt-6">
-    
+      <div className="w-full">
+        <div className="pt-6 w-full flex flex-col lg:flex-row">
 
-    {/* <!-- Image gallery --> */}
-    <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
-      <div className="aspect-h-4 aspect-w-3 hidden overflow-hidden rounded-lg lg:block">
-        <img src={producto?.imagenes?.principal} alt={producto?.nombre} className="h-full w-full object-cover object-center"/>
-      </div>
-    </div>
 
-    {/* <!-- Product info --> */}
-    <div className="mx-auto max-w-2xl px-4 pb-16 pt-10 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
-      <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
-        <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">{producto?.nombre}</h1>
-      </div>
-
-      {/* <!-- Options --> */}
-      <div className="mt-4 lg:row-span-3 lg:mt-0">
-        <h2 className="sr-only">Product information</h2>
-        <p className="text-3xl tracking-tight text-gray-900">${producto?.valor}</p>
-
-        {/* <!-- Reviews --> */}
-        <div className="mt-6">
-          <h3 className="sr-only">Reviews</h3>
-          <div className="flex items-center">
-            <div className="flex items-center">
-              {/* <!-- Active: "text-gray-900", Default: "text-gray-200" --> */}
-              <svg className="text-gray-900 h-5 w-5 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path fillRule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" clipRule="evenodd" />
-              </svg>
-              <svg className="text-gray-900 h-5 w-5 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path fillRule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" clipRule="evenodd" />
-              </svg>
-              <svg className="text-gray-900 h-5 w-5 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path fillRule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" clipRule="evenodd" />
-              </svg>
-              <svg className="text-gray-900 h-5 w-5 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path fillRule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" clipRule="evenodd" />
-              </svg>
-              <svg className="text-gray-200 h-5 w-5 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path fillRule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" clipRule="evenodd" />
-              </svg>
+          {/* <!-- Image gallery --> */}
+          <div className="mx-auto mt-6 w-full sm:px-6  lg:max-w-7xl lg:gap-x-8 lg:px-8 lg:w-1/2">
+            <div className="w-full rounded-lg">
+              <img src={producto?.imagenes?.principal} alt={producto?.nombre} className="w-full rounded-xl object-cover object-center " />
             </div>
-            <p className="sr-only">4 out of 5 stars</p>
-            <Link href="#" className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">117 reseñas</Link>
+          </div>
+
+          {/* <!-- Product info --> */}
+          <div className="mx-auto max-w-2xl px-4 pb-16 pt-5 sm:px-6 lg:max-w-7xl   lg:gap-x-8 lg:px-8 lg:pb-24 flex flex-col lg:w-1/2 gap-5">
+            <div className=" w-full lg:pr-8">
+              <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-3xl uppercase">{producto?.nombre}</h1>
+            </div>
+
+            {/* <!-- Options --> */}
+            <div className="mt-4 lg:row-span-3 lg:mt-0">
+              <h2 className="sr-only">Product information</h2>
+              <p className="text-4xl tracking-tight text-gray-900">${producto?.valor}</p>
+
+              {/* <!-- Reviews --> */}
+              <div className="mt-6">
+                <h3 className="sr-only">Reviews</h3>
+                <div className="flex items-center">
+                  <div className="flex items-center">
+                    {/* <!-- Active: "text-gray-900", Default: "text-gray-200" --> */}
+                    <svg className="text-gray-900 h-5 w-5 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path fillRule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" clipRule="evenodd" />
+                    </svg>
+                    <svg className="text-gray-900 h-5 w-5 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path fillRule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" clipRule="evenodd" />
+                    </svg>
+                    <svg className="text-gray-900 h-5 w-5 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path fillRule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" clipRule="evenodd" />
+                    </svg>
+                    <svg className="text-gray-900 h-5 w-5 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path fillRule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" clipRule="evenodd" />
+                    </svg>
+                    <svg className="text-gray-200 h-5 w-5 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path fillRule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" clipRule="evenodd" />
+                    </svg>
+                  </div>
+                  <p className="sr-only">4 out of 5 stars</p>
+                  <Link href="#" className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">117 reseñas</Link>
+                </div>
+              </div>
+
+              <form className="mt-10 flex flex-col lg:flex-row gap-5">
+
+
+
+                <button type="submit" className="flex w-full items-center justify-center rounded-md border border-indigo-700  px-8 py-3 text-base font-medium text-indigo-700 hover:bg-slate-100 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Agregar al carrito</button>
+                <button type="submit" className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Comprar ahora</button>
+              </form>
+            </div>
+
+            <div className="py-10 lg:col-span-2 lg:col-start-1 lg:pb-16 lg:pr-8 lg:pt-6">
+              {/* <!-- Description and details --> */}
+              <div>
+                <h3 className="sr-only">Description</h3>
+
+                <div className="space-y-6">
+                  <p className=" text-gray-900 text-xl">{producto?.descripcion}</p>
+                </div>
+              </div>
+
+
+
+
+            </div>
           </div>
         </div>
-
-        <form className="mt-10">
-        
-
-
-          <button type="submit" className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Agregar al carrito</button>
-        </form>
       </div>
-
-      <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6">
-        {/* <!-- Description and details --> */}
-        <div>
-          <h3 className="sr-only">Description</h3>
-
-          <div className="space-y-6">
-            <p className="text-base text-gray-900">{producto?.descripcion}</p>
-          </div>
-        </div>
-
-        
-
-        
-      </div>
-    </div>
-  </div>
-</div>
 
     </>
 
