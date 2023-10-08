ff07cd82cd4a132f32bcfb687067aeb0807a2a24
DanielaV26 <danielavargasgonzalez.v@gmail.com>
2023-10-08 00:53:52 -0300

    [fix]: estilos pagina nosotros e icono favoritos

--- src/Pages/CheckoutUserForm.jsx
+++ src/Pages/CheckoutUserForm.jsx
@@ -6,7 +6,7 @@ export const CheckoutUserForm = () => {
     return (
         <section className="flex gap-4 flex-col md:flex-row w-full md:w-auto">
             <form className="flex flex-col p-4 bg-white border drop-shadow-sm w-full gap-4 rounded-xl">
-                <h3 className="font-semibold font-montserrat text-lg">Inicia sesión</h3>
+                <h3 className="font-semibold text-lg">Inicia sesión</h3>
                 <Input
                     endContent={
                         <MailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
@@ -33,23 +33,23 @@ export const CheckoutUserForm = () => {
                         border: 'none'
                     }}
                 />
-                <a className="cursor-pointer underline ml-auto font-montserrat text-violet-500">Olvidé mi contraseña</a>
-                <button className="bg-gradient-to-r from-violet-500 to-fuchsia-500 text-white rounded-full p-2 font-semibold cursor-pointer font-montserrat" type="submit">
+                <a className="cursor-pointer underline ml-auto text-violet-500">Olvidé mi contraseña</a>
+                <button className="bg-gradient-to-r from-violet-500 to-fuchsia-500 text-white rounded-full p-2 font-semibold cursor-pointer" type="submit">
                     Iniciar sesión
                 </button>
             </form>
 
             <form className="flex flex-col p-4 bg-white w-full gap-4 rounded-xl border drop-shadow-sm">
-                <h3 className="font-semibold font-montserrat text-lg">Crea tu cuenta</h3>
-                <p className="font-montserrat">Con tu registro, accedes a la información de tus compras y obtienes múltiples beneficios.</p>
-                <ul className="font-montserrat">
+                <h3 className="font-semibold text-lg">Crea tu cuenta</h3>
+                <p>Con tu registro, accedes a la información de tus compras y obtienes múltiples beneficios.</p>
+                <ul className="italic">
                     <li>Conoce el estado de todas tus compras</li>
                     <li>Pide cambios, devoluciones y reembolsos.</li>
                     <li>Descarga tus boletas y mucho más.</li>
                 </ul>
-                <button className="bg-gradient-to-r from-violet-500 to-fuchsia-500 text-white rounded-full p-2 font-semibold cursor-pointer font-montserrat" type="submit">Crear mi cuenta </button>
+                <button className="bg-gradient-to-r from-violet-500 to-fuchsia-500 text-white rounded-full p-2 font-semibold cursor-pointer" type="submit">Crear mi cuenta </button>
                 <hr />
-                <p className="font-montserrat">También puedes <a className="underline cursor-pointer text-violet-500">comprar sin registro</a></p>
+                <p>También puedes <a className="underline cursor-pointer text-violet-500">comprar sin registro</a></p>
             </form>
         </section>
     )
