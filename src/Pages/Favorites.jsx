a5e8f4dd05ea54b107b114837f902fc81426ebac
Carolina Hernandez <hernandez.carolina@live.cl>
2023-10-12 15:36:16 -0300

    [feat]: imagen favoritos sin productos

--- src/Pages/Favorites.jsx
+++ src/Pages/Favorites.jsx
@@ -33,9 +33,15 @@ return (
     <>
       <h2 className="text-6xl h- lg:h-16 text-center font-cookie mx-auto bg-gradient-to-t from-violet-600 to-yellow-200 text-transparent bg-clip-text mt-8 ">Mis favoritos</h2>
       <div className="flex flex-wrap lg:flex-row justify-center items-center gap-3 w-full  ">
-        {obtenerFavoritos?.map((producto) => (
+        {obtenerFavoritos.length === 0? 
+        (<div className="flex flex-col justify-center items-center">
+          <img className="lg:w-40 w-36 mt-20" src="https://res.cloudinary.com/dt7h6qci4/image/upload/v1697129491/corazon_roto_gris_lfp6s3.png" alt="" />
+          <p className="mt-10 font-mooli text-xl p-5 lg:text-2xl text-center text-violet-400 ">Aún no tienes ningún producto en favoritos</p>
+          </div>):
+    
+        obtenerFavoritos?.map((producto) => (
     <ProductCard key={producto._id} producto={producto} />
-        ))}
+        ))  }
       </div>
     </>
 
