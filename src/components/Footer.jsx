7415d95126eca535569d01006ccc3507ed9d3dfd
Carolina Hernandez <hernandez.carolina@live.cl>
2023-10-12 09:43:04 -0300

    [fix]: Image product Page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,24 +9,40 @@ export const Footer = () => {
                 <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8 font-mooli">
                     <div className="md:flex flex-col md:justify-between">
                         <div className="mb-6 md:mb-0  flex justify-center mt-10 ">
-                                <img  src="https://res.cloudinary.com/dt7h6qci4/image/upload/v1696551338/brigthbook_titulo_png_bmkxc1_b8guto.png" className="h-20 mr-3" alt="Brightbooks Logo" />
+                                <img  src="https://res.cloudinary.com/dt7h6qci4/image/upload/v1696551338/brigthbook_titulo_png_bmkxc1_b8guto.png" className="lg:h-20 h-14 lg:mr-3" alt="Brightbooks Logo" />
                         </div>
-                        <div className="grid w-full  justify-around sm:flex md:flex md:grid-cols-1 font-mooli mt-10">
+                        <div className="mt-10 flex gap-5 lg:gap-10 justify-center mr-10 ">
+                            <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                                <svg className="w-8 h-8 lg:w-12 lg:h-12" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
+                                    <path fillRule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clipRule="evenodd" />
+                                </svg>
+                                <span className="sr-only">Facebook page</span>
+                            </a>
+                            <a href="https://instagram.com/brightbookss?igshid=MWZjMTM2ODFkZg==" target="_blank" className="w-8 lg:w-12 text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                                <InstagramSvg/>
+                                <span className="sr-only">Instagram</span>
+                            </a>
+                            <a href="https://wa.me/+56965678219?text=HOLA"  target="_blank" className="w-6 text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                                <WhatsappSvg className="w-8 lg:w-12" />
+                                <span className="sr-only ">WhatsApp</span>
+                            </a>
+                        </div>
+                        <div className="flex flex-col lg:flex-row lg:justify-around w-full  justify-center items-center font-mooli mt-10  lg:pr-28 ">
                             <div>
-                                <h2 className="mb-3 font-semibold text-violet-500  uppercase dark:text-white">Tienda física</h2>
+                                <h2 className="mb-3 ml-16 font-semibold text-violet-500  uppercase dark:text-white">Tienda física</h2>
                                 <ul className="text-gray-500 dark:text-gray-400 font-medium">
                                     <li className="mb-1">
                                         <a href="https://maps.app.goo.gl/Bu2oRYe7iPPEK8Hx9" target="_blank" className="hover:text-pink-400 text-sm">5 Norte 1500, Viña del Mar, Valparaíso.</a>
                                     </li>
                                     <li>
-                                        <span className="text-sm">Lunes a Sábado 10:30h - 19:30h</span>
+                                        <span className="text-sm ml-6">Lunes a Sábado 10:30h - 19:30h</span>
                                     </li>
                                 </ul>
                             </div>
                             <div>
-                                <h2 className="mb-3 font-semibold text-violet-500 uppercase dark:text-white">Contacto</h2>
+                                <h2 className="mb-3 font-semibold text-violet-500 uppercase dark:text-white ml-6 mt-5 ">Contacto</h2>
                                 <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                                    <li className="mb-1">
+                                    <li className="mb-1 ml-3">
                                         <a href="https://wa.me/+56965678219?text=HOLA"  target="_blank" className="hover:text-pink-400 text-sm">+569 6567 8219</a>
                                     </li>
                                     <li>
@@ -35,7 +51,7 @@ export const Footer = () => {
                                 </ul>
                             </div>
                             <div>
-                                <h2 className="mb-3 font-semibold text-violet-500 uppercase dark:text-white">Legal</h2>
+                                <h2 className="mb-3 font-semibold text-violet-500 uppercase dark:text-white ml-10 mt-3">Legal</h2>
                                 <ul className="text-gray-500 dark:text-gray-400 font-medium">
                                     <li className="mb-1">
                                         <a href="#" className="hover:text-pink-400 text-sm">Términos y condiciones</a>
@@ -48,25 +64,10 @@ export const Footer = () => {
                         </div>
                     </div>
                     <hr className="mb-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:mb-8 mt-6" />
-                    <div className="w-full flex flex-col sm:flex-row sm:items-center sm:justify-between justify-center">
-                        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 Brightbooks™. Todos los derechos reservados.
+                    <div className="w-full flex  lg:flex-row lg:items-center  justify-center">
+                        <span className="text-sm text-gray-500 text-center dark:text-gray-400">© 2023 Brightbooks™. Todos los derechos reservados.
                         </span>
-                        <div className="mt-4 flex gap-5 sm:mt-0 sm:justify-center ml-auto">
-                            <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-                                <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
-                                    <path fillRule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clipRule="evenodd" />
-                                </svg>
-                                <span className="sr-only">Facebook page</span>
-                            </a>
-                            <a href="https://instagram.com/brightbookss?igshid=MWZjMTM2ODFkZg==" target="_blank" className="w-6 text-gray-500 hover:text-gray-900 dark:hover:text-white">
-                                <InstagramSvg/>
-                                <span className="sr-only">Instagram</span>
-                            </a>
-                            <a href="https://wa.me/+56965678219?text=HOLA"  target="_blank" className="w-6 text-gray-500 hover:text-gray-900 dark:hover:text-white">
-                                <WhatsappSvg />
-                                <span className="sr-only">WhatsApp</span>
-                            </a>
-                        </div>
+                        
                     </div>
                 </div>
             </footer>
